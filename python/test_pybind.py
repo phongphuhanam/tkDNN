80d5cbe39ecd26044b6208c8bf0a89686aa4f2f7
Phu Ninh <18023194+phongphuhanam@users.noreply.github.com>
2020-06-15 23:45:26 +0900

    fix python examples

--- python/test_pybind.py
+++ python/test_pybind.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+lib_dir = os.path.dirname(os.path.abspath(__file__))
+lib_dir = os.path.join(lib_dir, '..', "build")
+print(lib_dir)
+sys.path.insert(0, lib_dir)
 import pytest
 from py_darknet import Tkdnn_darknet
 import cv2
